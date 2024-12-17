3ab9a685597c39c58807a23637b283194c89f002
Dincuy <66787508+dincuy@users.noreply.github.com>
2024-12-17 14:02:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
 
+class MyApp(App):
+    def build(self):
+        return Label(text="Hello, Kivy on Gitpod!")
+
+if __name__ == "__main__":
+    MyApp().run()
